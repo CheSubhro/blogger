bccb55685a70413a9e14170573e8a75c08de5d0f
CheSubhro <mitsubhra@gmail.com>
2024-03-07 13:06:30 +0530

    Set Routes

---
+++ server/src/routes/user.route.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+
+const router = Router()
+
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }, 
+    ]),
+    registerUser
+    )
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+
+export default router
